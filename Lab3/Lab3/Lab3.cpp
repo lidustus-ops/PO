9683b2f0398ecebc76fe5a5ad28440cdebe18514
lidustus-ops <lidustus@gmail.com>
2026-04-11 02:32:07 +0300

    change functional in main, add terminate()

--- Lab3/Lab3/Lab3.cpp
+++ Lab3/Lab3/Lab3.cpp
@@ -5,27 +5,35 @@
 #include <mutex>
 #include <condition_variable>
 #include <functional>
+#include <atomic>
 
 class ThreadPool {
 public:
-    ThreadPool(size_t threads) : m_stop(false) {
+    ThreadPool(size_t threads, size_t max_queue_size)
+        : m_max_queue_size(max_queue_size), m_stop(false) {
         for (size_t i = 0; i < threads; ++i) {
             m_workers.emplace_back(&ThreadPool::worker_routine, this);
         }
     }
 
-    ~ThreadPool() {
-        { std::lock_guard<std::mutex> lock(m_queue_mutex); m_stop = true; }
-        m_condition.notify_all();
-        for (std::thread& worker : m_workers) if (worker.joinable()) worker.join();
+    ~ThreadPool() { terminate(); }
+
+    bool add_task(std::function<void()> task) {
+        std::unique_lock<std::mutex> lock(m_queue_mutex);
+        if (m_tasks.size() >= m_max_queue_size) return false;
+        m_tasks.push(std::move(task));
+        m_condition.notify_one();
+        return true;
     }
 
-    void add_task(std::function<void()> task) {
+    void terminate() {
         {
             std::lock_guard<std::mutex> lock(m_queue_mutex);
-            m_tasks.push(std::move(task));
+            if (m_stop) return;
+            m_stop = true;
         }
-        m_condition.notify_one();
+        m_condition.notify_all();
+        for (std::thread& worker : m_workers) if (worker.joinable()) worker.join();
     }
 
 private:
@@ -47,12 +55,17 @@ private:
     std::queue<std::function<void()>> m_tasks;
     std::mutex m_queue_mutex;
     std::condition_variable m_condition;
-    bool m_stop;
+    size_t m_max_queue_size;
+    std::atomic<bool> m_stop;
 };
 
 int main() {
-    ThreadPool pool(4);
-    pool.add_task([] { std::cout << "Core pool is working\n"; });
+
+    const int threads = 6;
+    const int queue_limit = 15;
+    ThreadPool pool(threads, queue_limit);
+
+    pool.add_task([] { std::cout << "Thread pool core is active\n"; });
     std::this_thread::sleep_for(std::chrono::seconds(1));
     return 0;
 }
