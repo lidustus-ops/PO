3ca9d8128fdaa729fd23d7e4afb9d0f0de2e49df
lidustus-ops <lidustus@gmail.com>
2026-04-29 01:35:30 +0300

    Init network and structures

---
+++ Lab4/Client/Client/Client.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+
+#pragma comment(lib, "ws2_32.lib")
+
+using namespace std;
+
+#pragma pack(push, 1)
+struct DataHeader {
+    uint32_t matrix_size;
+    uint32_t thread_count;
+    uint32_t payload_size;
+};
+#pragma pack(pop)
+
+int main() {
+    WSADATA wsa;
+    WSAStartup(MAKEWORD(2, 2), &wsa);
+
+    SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
+    sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(55555);
+    inet_pton(AF_INET, "127.0.0.1", &addr.sin_addr);
+
+    if (connect(sock, (sockaddr*)&addr, sizeof(addr)) == SOCKET_ERROR) {
+        cout << "Server is offline." << endl;
+    }
+    else {
+        cout << "Connected to server!" << endl;
+    }
+
+    closesocket(sock);
+    WSACleanup();
+    return 0;
+}
---
+++ Lab4/Server/Server/Server.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <vector>
+#include <winsock2.h>
+
+#pragma comment(lib, "ws2_32.lib")
+
+using namespace std;
+
+#pragma pack(push, 1)
+struct DataHeader {
+    uint32_t matrix_size;
+    uint32_t thread_count;
+    uint32_t payload_size;
+};
+#pragma pack(pop)
+
+struct ClientSession {
+    vector<vector<int>> matrix;
+    int threads_to_use = 1;
+};
+
+int main() {
+    WSADATA wsa;
+    WSAStartup(MAKEWORD(2, 2), &wsa);
+
+    SOCKET srv = socket(AF_INET, SOCK_STREAM, 0);
+    sockaddr_in addr = { AF_INET, htons(55555), INADDR_ANY };
+
+    bind(srv, (sockaddr*)&addr, sizeof(addr));
+    listen(srv, SOMAXCONN);
+
+    cout << "Server initialized. Waiting for connections..." << endl;
+
+    while (true) {
+        SOCKET cl = accept(srv, nullptr, nullptr);
+        if (cl != INVALID_SOCKET) {
+            cout << "New client connected!" << endl;
+            closesocket(cl);
+        }
+    }
+
+    closesocket(srv);
+    WSACleanup();
+    return 0;
+}
