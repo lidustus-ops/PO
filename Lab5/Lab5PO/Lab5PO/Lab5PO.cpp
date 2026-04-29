c470a1bc553a7d706bbd68d996b48c651e0f531f
lidustus-ops <lidustus@gmail.com>
2026-04-29 09:05:35 +0300

    Complete lab 5

--- Lab5/Lab5PO/Lab5PO/Lab5PO.cpp
+++ Lab5/Lab5PO/Lab5PO/Lab5PO.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <fstream>
 #include <sstream>
+#include <thread>
 #include <winsock2.h>
 #include <ws2tcpip.h>
 
@@ -15,8 +16,14 @@ const int RECV_BUFFER = 1024;
 string load_file_content(const string& fileName) {
     string folder = "www";
     string fullPath = folder + fileName;
+
     ifstream targetFile(fullPath, ios::binary);
-    if (!targetFile.is_open()) return "";
+
+    if (!targetFile.is_open()) {
+        cout << "ERROR: Cannot find file at: " << fullPath << endl;
+        return "";
+    }
+
     stringstream buffer;
     buffer << targetFile.rdbuf();
     return buffer.str();
@@ -25,48 +32,68 @@ string load_file_content(const string& fileName) {
 void process_connection(SOCKET clientNode) {
     char incomingData[RECV_BUFFER] = { 0 };
     int result = recv(clientNode, incomingData, RECV_BUFFER, 0);
+
     if (result > 0) {
         istringstream requestStream(incomingData);
         string action, route, version;
         requestStream >> action >> route >> version;
+
         if (route == "/") route = "/index.html";
 
         string fileData = load_file_content(route);
         string finalResponse;
+
         if (!fileData.empty()) {
-            finalResponse = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n";
+            finalResponse = "HTTP/1.1 200 OK\r\n";
+            finalResponse += "Content-Type: text/html\r\n";
             finalResponse += "Content-Length: " + to_string(fileData.size()) + "\r\n";
-            finalResponse += "Connection: close\r\n\r\n" + fileData;
+            finalResponse += "Connection: close\r\n\r\n";
+            finalResponse += fileData;
         }
         else {
             string errorMsg = "<h1>404 - Storinka ne znaydena</h1>";
-            finalResponse = "HTTP/1.1 404 Not Found\r\nContent-Type: text/html\r\n";
-            finalResponse += "Content-Length: " + to_string(errorMsg.size()) + "\r\n\r\n" + errorMsg;
+            finalResponse = "HTTP/1.1 404 Not Found\r\n";
+            finalResponse += "Content-Type: text/html\r\n";
+            finalResponse += "Content-Length: " + to_string(errorMsg.size()) + "\r\n\r\n";
+            finalResponse += errorMsg;
         }
+
         send(clientNode, finalResponse.c_str(), (int)finalResponse.size(), 0);
     }
+
     closesocket(clientNode);
 }
 
 int main() {
     WSADATA wsa;
-    WSAStartup(MAKEWORD(2, 2), &wsa);
+    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) {
+        cout << "Error initializing WinSock" << endl;
+        return 1;
+    }
+
     SOCKET listener = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
     sockaddr_in service;
     service.sin_family = AF_INET;
     service.sin_addr.s_addr = INADDR_ANY;
     service.sin_port = htons(SERVER_PORT);
-    bind(listener, (struct sockaddr*)&service, sizeof(service));
-    listen(listener, SOMAXCONN);
 
+    if (bind(listener, (struct sockaddr*)&service, sizeof(service)) == SOCKET_ERROR) {
+        cout << "Bind failed" << endl;
+        return 1;
+    }
+
+    listen(listener, SOMAXCONN);
     cout << "Web-server is running on port " << SERVER_PORT << "..." << endl;
 
     while (true) {
         SOCKET session = accept(listener, NULL, NULL);
         if (session != INVALID_SOCKET) {
-            process_connection(session); 
+            thread worker(process_connection, session);
+            worker.detach();
         }
     }
+
     closesocket(listener);
     WSACleanup();
     return 0;
